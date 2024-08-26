0627fe1d5c66d95e1366e8d06e9a1e8af228c531
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-08-26 16:51:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		sourcemap: false,
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {

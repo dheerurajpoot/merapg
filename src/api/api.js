39bed85d8ae58685d9df1ce5c2bc154d20d6b39d
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-08-26 14:26:50 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,2 @@
-export const api = "https://merapg-server.onrender.com/api";
-export const BACKEND_URL = "https://merapg-server.onrender.com";
-// export const api = "http://localhost:3000/api";
-// export const BACKEND_URL = "http://localhost:3000";
+export const api = "https://api.merapg.com/api";
+export const BACKEND_URL = "https://api.merapg.com";

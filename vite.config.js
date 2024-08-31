77491d4c6abaf770e68817597dfacdd139aa9210
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-08-31 21:22:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { BACKEND_URL } from "./src/api/api";
 
 export default defineConfig({
 	build: {
 		sourcemap: false,
 	},
 	plugins: [react()],
+	server: {
+		proxy: {
+			"/sitemap.xml": BACKEND_URL,
+		},
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
